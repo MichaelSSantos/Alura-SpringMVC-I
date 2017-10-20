2f9b27ca17a0d8a92d72c1ec9433f44de8f9f272
MichaelSantos <MichaelSantos@10.50.20.9>
2017-10-20 00:23:43 -0200

    Inclusão do projeto.

---
+++ casadocodigo/src/main/java/casadocodigo/loja/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package casadocodigo.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller//Duas formas de configurar isso no Spring: Por annotation ou por xml.
+public class HomeController {
+
+	
+	
+}
